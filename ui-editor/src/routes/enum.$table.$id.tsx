ae64f7a844c20fc4d44b72bda07df0c4826e024d
Vyacheslav Voloshyn <vyacheslav.voloshyn@prusa3d.cz>
2025-12-19 09:52:55 +0100

    feat: add enums operations

--- ui-editor/src/routes/enum.$table.$id.tsx
+++ ui-editor/src/routes/enum.$table.$id.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import { Trash2 } from 'lucide-react';
 import React from 'react';
 
